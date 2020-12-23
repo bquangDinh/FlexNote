64cdaa8166fe9a01fc1ef419bdfed7e6f47d1251
Dinh Quang Bui <buiquangdinh1751@gmail.com>
2020-12-22 20:21:13 -0500

    Finished CSS module

--- src/components/custom-select/index.vue
+++ src/components/custom-select/index.vue
@@ -1,4 +1,4 @@
 <template src="./custom-select.html"></template>
 <script src="./custom-select.js"></script>
-<style src="./custom-select.scss" scoped lang="scss"></style>
+<style src="./custom-select.scss" module lang="scss"></style>
 
--- src/components/translate-widget/index.vue
+++ src/components/translate-widget/index.vue
@@ -1,4 +1,4 @@
 <template src="./translate-widget.html"></template>
 <script src="./translate-widget.js"></script>
-<style src="./translate-widget.scss" scoped lang="scss"></style>
+<style src="./translate-widget.scss" module lang="scss"></style>
 
